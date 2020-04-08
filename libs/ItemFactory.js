4d03bae2df428b38b778779ec5bc0ca2cc9b4033
Pieter Lewyllie <plewyllie@gmail.com>
2020-04-08 22:30:46 +0200

    jS...

--- libs/ItemFactory.js
+++ libs/ItemFactory.js
@@ -134,8 +134,9 @@ exports.Factory.prototype.parseSitemap = function(jsonSitemap) {
     console.log(JSON.stringify(this.knxScreens_Shared, null, 4));
     // PIETER addition: We are going through the list. Each EIBBlindsPositionItem has a link to it's EIBBlinds parent
     for (var room in this.knxScreens_Shared) {
-      room.position.parentBlind = room.updown;
+      console.log(JSON.stringify(room, null, 4));
       console.log(JSON.stringify(room.position, null, 4));
+      this.knxScreens_Shared[room].position.parentBlind = room.updown;
     }
 
     this.log('Platform - Total accessory count ' + accessoryList.length + ' across ' + this.platform.rooms.length + ' rooms.');
