1ff99e67bca42b2234c7039a5a6f3de64127b880
hihipto <hihipto@gmail.com>
2020-10-06 18:41:05 +0200

    re-enabled MotionSensor

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+//
+//     according example: https://github.com/homebridge/homebridge-plugin-template/blob/master/src/index.ts
+//
+import { API } from 'homebridge';
+
+import { PLATFORM_NAME } from './settings';
+import { ExampleHomebridgePlatform } from './platform';
+
+/**
+ * This method registers the platform with Homebridge
+ */
+export = (api: API) => {
+  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+};
+//
+//    end of example
+//
+//
 'use strict';
 
 var Homebridge, Accessory;
